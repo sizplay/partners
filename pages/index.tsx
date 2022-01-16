6d33b6bf52fd4f4eebb3273b47c2d889a2fe24cb
Chaehoon Lim <sizplay@gmail.com>
2022-01-16 10:15:12 +0900

    change init

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,13 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import styled from "styled-components";
 
+const Temp = styled.div`
+  background-color: red;
+  width: 100px;
+  height: 200px;
+`;
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
@@ -23,6 +29,7 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.grid}>
+          <Temp />
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
